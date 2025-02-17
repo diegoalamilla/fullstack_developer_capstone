17174681c45b71fd7ef9338a119809c1931f5cf5
diegoalamilla <diego.alamilla123@gmail.com>
2025-02-17 15:14:10 -0500

    fix syntax smells

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,21 +1,20 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-
-
 class CarMake(models.Model):
-    name= models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     description = models.TextField()
 
     def __str__(self):
         return self.name
 
+
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, null=True, on_delete = models.CASCADE)
-    name= models.CharField(max_length=100)
+    car_make = models.ForeignKey(CarMake, 
+    null=True, on_delete=models.CASCADE)
+    name=models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
